49526d65bd7f6d81059742d41ef44d4d1db720b2
Leonardo de Almeida <leodealmeida9@gmail.com>
2021-07-11 13:27:14 -0300

    feat(trade): add service to check when a trade is considered fair or not

---
+++ src/main/java/com/poketrader/api/application/trade/TradeFairnessCheckerAppService.java
@@ -0,0 +1,28 @@
+package com.poketrader.api.application.trade;
+
+import com.poketrader.api.presentation.PokemonResource;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TradeFairnessCheckerAppService {
+
+    private final int FAIRNESS_RATE = 30;
+
+    public boolean isTradeFair(List<PokemonResource> sideA, List<PokemonResource> sideB) {
+        Integer absDifference = evalAbsDifference(sideA, sideB);
+        return absDifference < FAIRNESS_RATE;
+    }
+
+    public Integer evalAbsDifference(List<PokemonResource> sideA, List<PokemonResource> sideB) {
+        int totalBaseExperienceSideA = sideA.stream()
+                .map(PokemonResource::getBase_experience)
+                .mapToInt(Integer::intValue).sum();
+        int totalBaseExperienceSideB = sideB.stream()
+                .map(PokemonResource::getBase_experience)
+                .mapToInt(Integer::intValue).sum();
+        return Math.abs(totalBaseExperienceSideA - totalBaseExperienceSideB);
+    }
+
+}
---
+++ src/test/java/com/poketrader/api/application/trade/TradeFairnessCheckerAppServiceTest.java
@@ -0,0 +1,56 @@
+package com.poketrader.api.application.trade;
+
+import com.poketrader.api.presentation.PokemonResource;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class TradeFairnessCheckerAppServiceTest {
+
+    @Autowired
+    TradeFairnessCheckerAppService tradeFairnessCheckerAppService;
+
+    @Test
+    void returnTrueWhenTradedPokemonIsFair() {
+        //setup
+        var ditto = new PokemonResource(132L, "ditto", 101);
+        var pikachu =  new PokemonResource(25L, "pikachu", 112);
+
+        ArrayList<PokemonResource> sideA = new ArrayList<>();
+        sideA.add(ditto);
+
+        ArrayList<PokemonResource> sideB = new ArrayList<>();
+        sideB.add(pikachu);
+
+        //execute
+        boolean isFair = tradeFairnessCheckerAppService.isTradeFair(sideA, sideB);
+
+        //verify
+        assertThat(isFair).isTrue();
+    }
+
+    @Test
+    void returnFalseWhenTradeIsNotFair() {
+        //setup
+        var lowBaseExperience = new PokemonResource(132L, "ditto", 2);
+        var highBaseExperience = new PokemonResource(25L, "pikachu", 999);
+
+        ArrayList<PokemonResource> sideA = new ArrayList<>();
+        sideA.add(lowBaseExperience);
+
+        ArrayList<PokemonResource> sideB = new ArrayList<>();
+        sideB.add(highBaseExperience);
+
+        //execute
+        boolean isFair = tradeFairnessCheckerAppService.isTradeFair(sideA, sideB);
+
+        //verify
+        assertThat(isFair).isFalse();
+    }
+
+}
